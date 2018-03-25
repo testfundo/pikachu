71aa416ea082f20e2d0c190cfe8fa0c76cdabf42
LENOVO <lenovo@localhost.localdomain>
2018-03-25 18:25:28 +0530

    poda

--- add_diary.php
+++ add_diary.php
@@ -1,12 +1,6 @@
 <?php
 
-/**
- * Copyright (c) 2007 Nathan Kinkade
- * 
- * This code is offered under an MIT (X11) license.  For more information
- * about the terms of this license see the file LICENSE included with this
- * software or visit: http://www.opensource.org/licenses/mit-license.php
- */
+
 
 # include the main site config where various global variables
 # and libraries are included
